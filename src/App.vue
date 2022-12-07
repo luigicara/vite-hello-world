559a92307b19c3c8cd7551b836edf1e26d94fe1e
luigicara <luigi.caravelli98@gmail.com>
2022-12-07 13:54:16 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import AppTitle from "./components/AppTitle.vue"
+
+export default {
+  components: {
+    AppTitle
+  }
+}
+</script>
+
+<template>
+  <AppTitle />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+}
+</script>
+
+<template>
+    <h1>La mia prima app con Vite!</h1>
+</template>
+
+<style scoped>
+h1 {
+    background-color: red;
+    text-align: center;
+}
+</style>
